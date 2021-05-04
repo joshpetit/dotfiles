f1ee36ab4d147bb169eb56cf10c8f7fd7e027556
joshu <joshuapetitma@yahoo.com>
2021-05-04 17:01:14 -0400

    Add basic polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload example &
+done
+
+# polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+
+echo "Bars launched..."
