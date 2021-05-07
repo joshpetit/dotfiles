f3ae845c2c8aa6607581ebb80d65a0c8d7ec9606
joshu <joshuapetitma@yahoo.com>
2021-05-06 22:15:17 -0500

    Fix things

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,21 +4,15 @@ dir="$HOME/.config/polybar"
 themes=(`ls --hide="launch.sh" $dir`)
 
 launch_bar() {
-	# Terminate already running bar instances
-	killall -q polybar
+    # Terminate already running bar instances
+    killall -q polybar
 
-	# Wait until the processes have been shut down
-	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+  # Wait until the processes have been shut down
+  while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-	# Launch the bar
-	if [[ "$style" == "hack" || "$style" == "cuts" ]]; then
-		polybar -q top -c "$dir/$style/config.ini" &
-		polybar -q bottom -c "$dir/$style/config.ini" &
-	elif [[ "$style" == "pwidgets" ]]; then
-		bash "$dir"/pwidgets/launch.sh --main
-	else
-		polybar --reload -q main -c "$dir/$style/config.ini" &	
-	fi
+  for m in $(polybar --list-monitors | cut -d":" -f1); do
+      MONITOR=$m polybar --reload -q main -c "$dir/$style/config.ini" &
+  done
 }
 
 if [[ "$1" == "--material" ]]; then
