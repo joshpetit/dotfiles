433ba519fb269e68ad62e44959ddf8ffd516d1e2
Joshua Petitma <joshua@petit.dev>
2021-12-22 11:12:55 -0500

    make cool changes

---
+++ .config/nvim/lua/mystuff/debug/init.lua
@@ -0,0 +1,16 @@
+M = {}
+
+M.node = function()
+    print("attaching")
+    require('dap').run({
+        type = 'node2',
+        request = 'attach',
+        cwd = vim.fn.getcwd(),
+        sourceMaps = true,
+        protocol = 'inspector',
+        skipFiles = {'<node_internals>/**/*.js'}
+    })
+    require("dapui").open();
+end
+
+return M;
