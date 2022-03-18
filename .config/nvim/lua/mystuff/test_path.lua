b74d6ab1625425fcefe6150937f7f97e73eb2036
Joshua Petitma <joshua@petit.dev>
2022-03-18 10:41:37 -0400

    Lua diagnostic

--- .config/nvim/lua/mystuff/test_path.lua
+++ .config/nvim/lua/mystuff/test_path.lua
@@ -4,7 +4,7 @@ M.dart = function()
 	local file = vim.fn.expand("%")
 
     local testFile = file:gsub("lib/src", "test/src")
-    local testFile = testFile:gsub(".dart", "_test.dart")
+    testFile = testFile:gsub(".dart", "_test.dart")
 	vim.cmd("split " .. testFile)
 
 end
