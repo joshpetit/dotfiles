a126e0aa4abfc56b8e9b7b9267fc7f9682333c14
Joshua Petitma <joshua@petit.dev>
2022-05-11 22:29:45 -0700

    Move prettier formatter

--- .config/nvim/lua/mystuff/plugin_conf/formatter-nvim/init.lua
+++ .config/nvim/lua/mystuff/plugin_conf/formatter-nvim/init.lua
@@ -1,3 +1,18 @@
+local prettierFormatter = {
+    -- prettier
+    function()
+        return {
+            exe = "prettier",
+            args = {
+                "--stdin-filepath",
+                vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)),
+                "--single-quote",
+            },
+            stdin = true,
+        }
+    end,
+}
+
 
 require("formatter").setup({
 	filetype = {
