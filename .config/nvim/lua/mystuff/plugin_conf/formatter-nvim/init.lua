46d3ad32d1f9d0d66459cb06e94be18b7ba8d338
Joshua Petitma <joshua@petit.dev>
2022-05-11 22:52:42 -0700

    formatting

--- .config/nvim/lua/mystuff/plugin_conf/formatter-nvim/init.lua
+++ .config/nvim/lua/mystuff/plugin_conf/formatter-nvim/init.lua
@@ -13,35 +13,34 @@ local prettierFormatter = {
     end,
 }
 
-
 require("formatter").setup({
-	filetype = {
-		jsonc = prettierFormatter,
-		markdown = prettierFormatter,
-		["creole.markdown"] = prettierFormatter,
-		lua = {
-			-- luafmt
-			function()
-				return { exe = "stylua", stdin = true, args = { "-" } }
-			end,
-		},
-		dart = {
-			-- Shell Script Formatter
-			function()
-				return { exe = "dart", args = { "format" }, stdin = true }
-			end,
-		},
-		-- org = {
-		--     -- Shell Script Formatter
-		--     function()
-		--         return {exe = "go-org", args = {"render org"}, stdin = true}
-		--     end
-		-- },
-		racket = {
-			-- Shell Script Formatter
-			function()
-				return { exe = "raco ", args = { "fmt", "--width 80" }, stdin = true }
-			end,
-		},
-	},
+    filetype = {
+        jsonc = prettierFormatter,
+        markdown = prettierFormatter,
+        ["creole.markdown"] = prettierFormatter,
+        lua = {
+            -- luafmt
+            function()
+                return { exe = "stylua", stdin = true, args = { "-" } }
+            end,
+        },
+        dart = {
+            -- Shell Script Formatter
+            function()
+                return { exe = "dart", args = { "format" }, stdin = true }
+            end,
+        },
+        -- org = {
+        --     -- Shell Script Formatter
+        --     function()
+        --         return {exe = "go-org", args = {"render org"}, stdin = true}
+        --     end
+        -- },
+        racket = {
+            -- Shell Script Formatter
+            function()
+                return { exe = "raco ", args = { "fmt", "--width 80" }, stdin = true }
+            end,
+        },
+    },
 })
