45327066d08094eb67850747c7b2adf7329edbbf
Joshua Petitma <joshua@petit.dev>
2022-07-06 15:09:12 -0700

    Make it doper

--- .local/share/scripts/sfmon-both-on.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output HDMI-0 --off --output DP-3 --mode 1920x1080 --pos 0x2160 --rotate normal --output DP-4 --mode 3840x2160 --pos 1920x0 --rotate normal
