64c0e64dd6e012f6c8666307006782540182b972
Joshua Petitma <joshua@petit.dev>
2022-07-17 00:13:31 -0700

    I like this a lot

---
+++ .config/polybar/theos/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/polybar/theos"
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch the bar
+polybar --reload -q workspaces -c "$DIR"/config.ini && xdo raise -N Polybar
+polybar --reload -q sysinfo -c "$DIR"/config.ini && xdo raise -N Polybar
