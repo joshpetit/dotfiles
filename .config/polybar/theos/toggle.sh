019ae80270b1c13134232735872787b32b7dde0c
Joshua Petitma <joshua@petit.dev>
2022-08-06 21:17:29 -0400

    Create a better toggle script!

--- .config/polybar/theos/toggle.sh
+++ .config/polybar/theos/toggle.sh
@@ -1,4 +1,32 @@
-polybar-msg cmd toggle 
+#polybar-msg cmd toggle 
 
 # Probably will be important
-# xprop _NET_WM_PID | sed 's/_NET_WM_PID(CARDINAL) = //'
+ #xprop _NET_WM_PID | sed 's/_NET_WM_PID(CARDINAL) = //'
+all_visible=$(xdotool search --onlyvisible --name polybar-sysinfo)
+
+org_bar=$(xdotool search --name polybar-org-timer getwindowpid)
+workspaces_bar=$(xdotool search --name polybar-workspaces getwindowpid)
+sysinfo_bar=$(xdotool search --name polybar-sysinfo getwindowpid)
+
+polybar-msg -p "$workspaces_bar" cmd toggle
+polybar-msg -p "$sysinfo_bar" cmd toggle
+
+org_active=$(emacsclient --eval "(org-clock-is-active)")
+
+toggle_to="hide"
+
+if [ "$all_visible" = "" ]
+then
+    toggle_to="show"
+fi
+
+if [  "$org_active" = "nil" ]
+then
+    polybar-msg -p "$org_bar" cmd hide
+else
+    polybar-msg -p "$org_bar" cmd $toggle_to
+fi
+
+
+
+xdo raise -N Polybar
