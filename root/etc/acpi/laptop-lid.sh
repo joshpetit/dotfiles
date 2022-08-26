00648d26c8e68ee2da7b229339ed617763f64902
Joshua Petitma <joshua@petit.dev>
2022-08-26 12:55:35 -0400

    awesome

--- root/etc/acpi/laptop-lid.sh
+++ root/etc/acpi/laptop-lid.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 source /home/joshu/.Xdbus
+current=$(su -c 'DISPLAY=:0.0 autorandr --current' joshu)
 grep -q closed /proc/acpi/button/lid/LID/state
+# It is closed
 if [ $? == 0 ]
 then
     # close action
-    current=$(su -c 'DISPLAY=:0.0 autorandr --current' joshu)
     if [ "$current" == "laptop" ]
     then
-        echo "CLOSED LAPTOP WITH ONE MONITOR"
+        echo "CLOSED LAPTOP WITH NO MONITORS"
         systemctl suspend
+    fi
+    if [ "$current" == "swift-normal" ]
+    then
+        echo "OPEND LAPTOP WITH MONITORS"
+        #su -c 'DISPLAY=:0.0 autorandr --load monitors-only' joshu
+    fi
+else
+    if [ "$current" == "monitors-only" ]
+    then
+        echo "OPENED LAPTOP WITH MONITORS"
+        #su -c 'DISPLAY=:0.0 autorandr --load swift-normal' joshu
     fi
 fi
