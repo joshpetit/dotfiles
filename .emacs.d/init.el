814c7a8aad5961c39fd6db8f6899f41cdb3624e2
Joshua Petitma <joshua@petit.dev>
2022-09-22 20:09:39 -0400

    Delete async shell to string

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -103,32 +103,6 @@
 ;(advice-add org-refile :after 'org-save-all-org-buffers)
 
 
-(defun async-shell-command-to-string (command callback)
-  "Execute shell command COMMAND asynchronously in the
-  background.
-
-  Return the temporary output buffer which command is writing to
-  during execution.
-
-  When the command is finished, call CALLBACK with the resulting
-  output as a string."
-  (let
-    ((output-buffer (generate-new-buffer " *temp*"))
-     (callback-fun callback))
-    (set-process-sentinel
-      (start-process "Shell" output-buffer shell-file-name shell-command-switch command)
-      (lambda (process signal)
-        (when (memq (process-status process) '(exit signal))
-          (with-current-buffer output-buffer
-                               (let ((output-string
-                                       (buffer-substring-no-properties
-                                         (point-min)
-                                         (point-max))))
-                                 (funcall callback-fun (string-trim output-string))))
-          (kill-buffer output-buffer))))
-    output-buffer))
-
-
 (setq org-confirm-babel-evaluate 0)
 (org-babel-do-load-languages
   'org-babel-load-languages (quote (
